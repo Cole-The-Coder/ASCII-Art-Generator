8b2efc4746104b3a1c5c637b67e51ea0cc056814
Cole Tynan <c0l3tw@gmail.com>
2022-01-27 14:27:17 -0800

    1st commit post cleanup

---
+++ backend/ascii-app/src/main/java/com/cole/asciiapp/AsciiAppApplication.java
@@ -0,0 +1,13 @@
+package com.cole.asciiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsciiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsciiAppApplication.class, args);
+	}
+
+}
