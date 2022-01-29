314d7a7f0e066100348c6a99f1da63d65bff37bf
Cole Tynan <c0l3tw@gmail.com>
2022-01-29 14:42:17 -0800

    Revert "Revert "1st commit post cleanup""
    This reverts commit 72c35d87ba33be08d30dff6789bca239c75dcd8a.

---
+++ backend/ascii-app/src/test/java/com/cole/asciiapp/AsciiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cole.asciiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsciiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
